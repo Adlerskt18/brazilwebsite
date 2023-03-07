641a574e726d84d2c131446a42bf9c8a8f46d164
Adlerskt18 <125394284+Adlerskt18@users.noreply.github.com>
2023-03-07 05:15:20 -0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Matches
 from django.db.models import Q
@@ -8,16 +9,22 @@ def index(request):
     return render(request, 'base/index.html')
 
 def resultPage(request):
-    q = request.GET.get("q")
+    q = request.GET.get('q') if request.GET.get('q') != "None" else ''
     team = Matches.objects.filter(
-        Q(match__icontains=q)
-        )
+            Q(match__icontains=q)
+            )
     wins = Matches.objects.filter(
-        Q(match__icontains=q) &
-        Q(result__icontains="W")
-        )
-    result = round((len(wins) / len(team)) * 100)
-    context = {'result':result}
+            Q(match__icontains=q) &
+            Q(result__icontains="W")
+            )
+    context = {}
+    if team and q != '':
+        result = round((len(wins) / len(team)) * 100)
+        context = {'result':result}
+    else:
+        messages.error(request, "Error")
+        return redirect('index')
+    
     return render(request, 'base/result_page.html', context)
 
 def aboutPage(request):
